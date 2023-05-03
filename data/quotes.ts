70dbc03f1d0e89d07cddb8e3685cbe620c691f68
David Johnson <david.johnson@flutterwavego.com>
2023-05-03 16:16:26 +0100

    feat(quotes.ts): add quotes data for application

---
+++ data/quotes.ts
@@ -0,0 +1,42 @@
+export const QUOTES = [
+  {
+    quote: "Be the change you wish to see in the world.",
+    author: "Mahatma Gandhi",
+  },
+  {
+    quote: "You miss 100% of the shots you don't take.",
+    author: "Wayne Gretzky",
+  },
+  {
+    quote:
+      "In three words I can sum up everything I've learned about life: it goes on.",
+    author: "Robert Frost",
+  },
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "The best way to predict the future is to invent it.",
+    author: "Alan Kay",
+  },
+  {
+    quote:
+      "The only thing necessary for the triumph of evil is for good men to do nothing.",
+    author: "Edmund Burke",
+  },
+  {
+    quote: "If you want to lift yourself up, lift up someone else.",
+    author: "Booker T. Washington",
+  },
+  {
+    quote:
+      "Happiness is not something ready made. It comes from your own actions.",
+    author: "Dalai Lama",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+];
